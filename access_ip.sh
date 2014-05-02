2731f38d55ae072a9411eade7b41c8c285af5118
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-05-02 21:54:17 +0900

    to empty master

--- access_ip.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-alias expand_nginx_log="( cat /var/log/nginx/access.log /var/log/nginx/access.log.1 ; gunzip -c /var/log/nginx/access.log.*.gz )"
-for ip in $(expand_nginx_log | nginx-log-analyzer 1 | sort -u) ; do 
-	whois $ip | grep 'country:' | head -n1 | awk '{print $2}' ;
-done | tr "[:lower:]" "[:upper:]" | sort | uniq -c | awk '{print $2 "," $1}' > access_ip.csv
--- access_number.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-alias expand_nginx_log="( cat /var/log/nginx/access.log /var/log/nginx/access.log.1 ; gunzip -c /var/log/nginx/access.log.*.gz )"
-expand_nginx_log | nginx-log-analyzer 4 | sed -e s'|^\(.*\)/\(.*\)/\([^:]*\):|\3/\2/\1:|' | sed -e s'|:.*$||' | sort | uniq -c | awk '{print $2 "," $1}' > access_number.csv
