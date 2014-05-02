38dbf2cae504ffddd30e8ac7a46ee60dc95c6d17
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-05-03 07:38:21 +0900

    re calculation

--- access_device.sh
+++ access_device.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 alias expand_nginx_log="( cat /var/log/nginx/access.log /var/log/nginx/access.log.1 ; gunzip -c /var/log/nginx/access.log.*.gz )"
-expand_nginx_log | nginx-log-analyzer 9 | sort | uniq -c | sed -e s'/^ *\([0-9]\+\) *\([^ ].*[^ ]\) *$/\2,\1/' > access_device.csv
+expand_nginx_log | nginx-log-analyzer 9 | sort | uniq -c | sed -e s'/^ *\([0-9]\+\) *\([^ ].*[^ ]\) *$/\2\t\1/' > access_device.tsv
